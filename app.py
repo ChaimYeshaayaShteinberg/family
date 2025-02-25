fed14a1ebdc2672e39d18345eca69f79130da797
Chaim Yesha'aye Shteinberg <a0533197133@gmail.com>
2025-02-25 15:38:23 +0200

    add_family_member.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -9,3 +9,12 @@ family = []
 @app.route('/')
 def home():
     return jsonify(family)
+
+@app.route('/add_family_member', methods=['POST'])
+def add_family_member():
+    data = request.get_json()
+    family.append(data)
+    return jsonify(family)
+
+if __name__ == '__main__':
+    app.route(debug=True)
