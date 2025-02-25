ec0fae4975083a96dca7d89a44a1d7ed11bab316
Chaim Yesha'aye Shteinberg <a0533197133@gmail.com>
2025-02-25 15:20:47 +0200

    homa.js

---
+++ script.js
@@ -0,0 +1,21 @@
+window.onload = function () {
+  fetch("http://127.0.0.1:5000/")
+    .then((response) => response.json())
+    .then((family) => {
+      const table = document.getElementById("familyTable");
+      const empty = document.getElementById("emptyFamily");
+      if (family.length > 0) {
+        table.style.display = "block";
+        family.forEach(member => {
+          const row = table.insertRow();
+          const typeCell = row.insertCell(0);
+          const nameCell = row.insertCell(1);
+          typeCell.textContent = member.type;
+          nameCell.textContent = member.name;
+        });
+      } else {
+        table.style.display = "none";
+        empty.style.display = "block";
+      }
+    });
+};
